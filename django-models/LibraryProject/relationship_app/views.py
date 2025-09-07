896de6adb9cfda5779ad26bc2d9ea6ca7955c378
ESTHER UVO <estheruvo@yahoo.com.com>
2025-09-07 12:44:34 -0400

    Fix views: add LibraryDetailView and import Library; add templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # ✅ Make sure Library is imported
 
-# Function-based view
+# Function-based view for listing all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view
+# Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  # <-- must match folder
+    template_name = 'relationship_app/library_detail.html'  # must match folder structure
     context_object_name = 'library'
+
+    # Add books to context so template can list them
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = Book.objects.filter(library=self.object)
+        return context
