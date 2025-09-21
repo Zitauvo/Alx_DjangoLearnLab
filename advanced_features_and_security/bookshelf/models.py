28e3692efe0ef7d0651b2d33f161ee9c0c2db39b
ESTHER UVO <estheruvo@yahoo.com.com>
2025-09-20 20:02:25 -0400

    Added Book model and registered in admin

--- advanced_features_and_security/bookshelf/models.py
+++ advanced_features_and_security/bookshelf/models.py
@@ -5,7 +5,12 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.PositiveIntegerField(null=True, blank=True)
-    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.title
